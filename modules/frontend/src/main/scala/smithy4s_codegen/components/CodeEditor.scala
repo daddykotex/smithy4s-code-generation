083f244d026005872ab518d3364c21d27f5e2f2c
David Francoeur <dfrancoeur04@gmail.com>
2023-05-01 16:31:47 -0400

    Improve styling of the code block

--- modules/frontend/src/main/scala/smithy4s_codegen/components/CodeEditor.scala
+++ modules/frontend/src/main/scala/smithy4s_codegen/components/CodeEditor.scala
@@ -32,7 +32,7 @@ class CodeEditor() {
     div(
       cls := "h-full",
       textArea(
-        cls := "block p-2.5 w-full h-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+        cls := "block p-2.5 w-full h-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500",
         value := initial,
         onMountFocus,
         onInput.mapToValue --> codeContent
